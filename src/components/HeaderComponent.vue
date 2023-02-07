737ca0551defa4cf97c84211ccba66616b38a887
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-02-07 17:18:48 +0100

    add-transition-navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,13 @@
 <template>
-    <nav class="navbar bg-white nav-position" v-show="showNav">
-        <div class="container-lg">
-            <a class="navbar-brand" href="#">
-                <img src="../assets/img/dishdrop-nero-arancione.png" alt="logo" width="80" height="80">
-            </a>
-        </div>
-    </nav>
+    <Transition name="scroll">
+        <nav class="navbar bg-white nav-position" v-show="showNav">
+            <div class="container-lg">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/dishdrop-nero-arancione.png" alt="logo" width="80" height="80">
+                </a>
+            </div>
+        </nav>
+    </Transition>
 </template>
 
 <script>
@@ -55,6 +57,16 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/styles/partials/__variables.scss' as *;
 
+
+.scroll-enter-active,
+.scroll-leave-active {
+    transition: all 0.6s cubic-bezier(.215, .61, .355, 1);
+}
+
+.scroll-enter-from,
+.scroll-leave-to {
+    transform: translateY(-100%);
+}
 
 .nav-position {
     position: fixed;
