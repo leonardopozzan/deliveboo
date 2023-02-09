36abe261be586ddcbbb8c282b747ff60c3298705
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-02-09 22:47:05 +0100

    call axios to menu page

---
+++ src/components/RedComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="jumbo-restaurant">
+        <div class="container">
+            <img class="img-logo ms-4" src="../assets/img/dishdrop-bianco-arancione.png" alt="">
+        </div>
+        <div class="restaurant-name">
+            <div class="container">
+
+            </div>
+        </div>
+    </div>
+
+
+
+</template>
+
+<script>
+import axios from 'axios';
+import{store} from '../store';
+    export default {
+       data(){
+        return{
+            store,
+            
+        }
+       },
+      
+    
+    }
+        
+       
+       
+    
+
+</script>
+
+<style lang="scss" scoped>
+
+.jumbo-restaurant{
+    background-color: red;
+    width: 100%;
+    position:relative;
+    .img-logo{
+        width:70px;
+        
+    }
+    
+
+}
+
+</style>
--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
-        Menu
+    <HeaderComponent></HeaderComponent>
+<RedComponent></RedComponent>
     </div>
 </template>
 
 <script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+import RedComponent from '../components/RedComponent.vue';
+import axios from 'axios';
+import {store} from '../store';
+
+
 export default {
-    name: 'Menu',
+    name: "Menu",
+    components: { HeaderComponent, RedComponent },
+    data(){
+        return{
+            store,
+        }
+    },
+    mounted(){
+        axios.get(`${this.store.apiBaseUrl}/restaurants/${this.$route.params.slug}`).then((response)=>{
+            console.log(response.data.results);
+            if(response.data.success){
+                this.restaurant=response.data.results;
+
+            }else{
+                this.$forceUpdate.push({name: 'notfound'});
+            }
+        });
+    }
+
 }
 </script>
 
