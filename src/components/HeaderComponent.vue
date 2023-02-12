48aa11295f09afd7df3e201939df7721555476a5
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-02-12 01:02:33 +0100

    ...Buonanotte

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,92 +1,93 @@
 <template>
-    <Transition name="scroll">
-        <nav class="navbar bg-white nav-position d-flex" v-show="showNav"
-            :class="{ 'nav-block': $route.name != 'home'  }">
-            <div class="container-lg">
-                <router-link to="/" class="navbar-brand">
-                    <img src="img/dish-drop-rb-01.png" alt="logo" width="80" height="80">
-                </router-link>
-                <div>
-                <router-link to="/restaurants">TUTTI I RISTORANTI </router-link>
-                
-                
-            </div>
-            </div>
-           
-        </nav>
-    </Transition>
+  <Transition name="scroll">
+    <nav
+      class="navbar bg-white nav-position d-flex"
+      v-show="showNav"
+      :class="{ 'nav-block': $route.name != 'home' }"
+    >
+      <div class="container-lg">
+        <router-link to="/" class="navbar-brand">
+          <img
+            src="img/dish-drop-rb-01.png"
+            alt="logo"
+            width="80"
+            height="80"
+          />
+        </router-link>
+        <div class="navbar-menu  pippo " >
+          <router-link to="/restaurants">Tutti i Ristoranti </router-link>
+          <router-link to="/reviewers">Scrivici una recenzione</router-link>
+        </div>
+      </div>
+      
+    </nav>
+  </Transition>
 </template>
 
 <script>
 export default {
-    name: 'HeaderComponent',
+  name: "HeaderComponent",
+
+  data() {
+    return {
+      showNav: false,
+      
+    };
+  },
+
+  mounted() {
+    if (this.$route.name === "home") {
+      window.addEventListener("scroll", this.handleScroll);
+    } else {
+      this.showNav = true;
+    }
+   ;
+   
+
+  },
 
 
-    data() {
-        return {
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+    
+  },
 
-            showNav: false,
+  computed: {},
 
-        }
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 250) {
+        this.showNav = true;
+      } else {
+        this.showNav = false;
+      }
     },
-
-
-    mounted() {
-        if (this.$route.name === 'home') {
-            window.addEventListener('scroll', this.handleScroll);
-        } else {
-            this.showNav = true;
-        }
-    },
-
-
-    beforeDestroy() {
-        window.removeEventListener('scroll', this.handleScroll);
-    },
-
-    computed: {
-    },
-
-
-    methods: {
-        handleScroll() {
-
-            if (window.scrollY > 250) {
-                this.showNav = true;
-            } else {
-                this.showNav = false;
-            }
-
-        }
-
-    },
-
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@use '../assets/styles/partials/__variables.scss' as *;
-
+@use "../assets/styles/partials/__variables.scss" as *;
 
 .scroll-enter-active,
 .scroll-leave-active {
-    transition: all 0.6s cubic-bezier(.215, .61, .355, 1);
+  transition: all 0.6s cubic-bezier(0.215, 0.61, 0.355, 1);
 }
 
 .scroll-enter-from,
 .scroll-leave-to {
-    transform: translateY(-100%);
+  transform: translateY(-100%);
 }
 
 .nav-position {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 10000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10000;
 }
 
 .nav-block {
-    position: sticky;
+  position: sticky;
 }
 </style>
